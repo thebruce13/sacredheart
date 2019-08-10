17f47d75028edf6cfdd1e26d30f11fa7abe57921
coobru@gmail.com <coobru@gmail.com>
2019-08-09 22:32:32 -0400

    update teacher layouts

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
  require_once( 'functions/starter.php' );
- require_once( 'functions/assets.php ');
- require_once( 'functions/yoast.php ');
+ require_once( 'functions/assets.php');
+ require_once( 'functions/yoast.php');
  require_once( 'functions/post_type_teacher.php');
- require_once( 'functions/taxonomy_class.php ');
+ require_once( 'functions/taxonomy_class.php');
